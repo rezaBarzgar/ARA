11e60e987bc1fb1053792444c32d85070ce39685
REZA <r.barzgar98@gmail.com>
2020-06-10 19:18:07 +0430

    testing

--- app/src/androidTest/java/com/a/ara/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/a/ara/ExampleInstrumentedTest.java
@@ -19,6 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
+        //this is a test
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.a.ara", appContext.getPackageName());
