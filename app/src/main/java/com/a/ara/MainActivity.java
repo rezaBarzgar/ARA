cce4469957ca9b0c7cc4d349fe47741da4cf841d
REZA <r.barzgar98@gmail.com>
2020-07-02 09:44:54 +0430

    phase 2

--- app/src/main/java/com/a/ara/MainActivity.java
+++ app/src/main/java/com/a/ara/MainActivity.java
@@ -2,7 +2,11 @@ package com.a.ara;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,28 +25,70 @@ import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     dbHelper dbh;
+    EditText et_username,et_password;
+    SharedPreferences preferences;
+    String current_user = "";
 
-
+    public static final int sign_up_req_code = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        TextView login_result = (TextView) findViewById(R.id.login_result);
-        Button sign_up = (Button) findViewById(R.id.btn_sign_up);
-        Button login = (Button) findViewById(R.id.btn_login);
 
         dbh = new dbHelper(this,"sho");
+
+        preferences = getSharedPreferences("locals",MODE_PRIVATE);
+        current_user = preferences.getString("username","***");
+        if (current_user.equals("***")){
+            setContentView(R.layout.activity_main);
+            initviews();
+            Toast.makeText(this, "Login First", Toast.LENGTH_SHORT).show();
+        }else {
+            setContentView(R.layout.main_page);
+            init_main_page_views();
+            Toast.makeText(this, "logged in as : " + current_user, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void init_main_page_views() {
+        TextView show_info = (TextView) findViewById(R.id.tv_show_info);
+        show_info.setText(preferences.getString(user.key_user_name,"NOT FOUND") +
+                "\n" + preferences.getString(user.key_user_first_name,"NOT FOUND") +
+                " " + preferences.getString(user.key_user_last_name,"NOT FOUND") +
+                "\n" + preferences.getString(user.key_user_email,"NOT FOUND") +
+                "\n" + preferences.getString(user.key_user_region,"NOT FOUND"));
+    }
+
+    private void initviews() {
+
+        final TextView login_result = (TextView) findViewById(R.id.login_result);
+        Button sign_up = (Button) findViewById(R.id.btn_sign_up);
+        Button login = (Button) findViewById(R.id.btn_login);
+        et_username = (EditText) findViewById(R.id.et_username_2);
+        et_password = (EditText) findViewById(R.id.et_password_2);
+
+        login_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                login_result.setTextColor(Color.rgb(random.nextInt(256),
+                        random.nextInt(256),random.nextInt(256)));
+            }
+        });
+
         SQLiteDatabase db = dbh.getWritableDatabase();
         Log.i("dbResult" , "database opened");
         dbh.onUpgrade(db,db.getVersion(),db.getVersion());
         int v = db.getVersion();
-        Toast.makeText(this, String.valueOf(v), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, String.valueOf(v), Toast.LENGTH_SHORT).show();
         if (db != null && db.isOpen()){
             db.close();
             Log.i("dbResult" , "database closed");
@@ -50,25 +97,40 @@ public class MainActivity extends AppCompatActivity {
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
-//                addUser();
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivityForResult(intent,sign_up_req_code);
             }
         });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
     }
 
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add("add users").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                addUser();
-                return false;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == sign_up_req_code){
+            if (resultCode == RESULT_OK){
+                Bundle result_bnd = data.getBundleExtra("result_sign_up");
+
+                preferences.edit().putString(user.key_user_name, result_bnd.getString(user.key_user_name)).apply();
+                preferences.edit().putInt(user.key_user_id, result_bnd.getInt(user.key_user_id)).apply();
+                preferences.edit().putString(user.key_user_first_name, result_bnd.getString(user.key_user_first_name)).apply();
+                preferences.edit().putString(user.key_user_last_name, result_bnd.getString(user.key_user_last_name)).apply();
+                preferences.edit().putString(user.key_user_email, result_bnd.getString(user.key_user_email)).apply();
+                preferences.edit().putString(user.key_user_region, result_bnd.getString(user.key_user_region)).apply();
+
+                setContentView(R.layout.main_page);
+                init_main_page_views();
+
+            }else if (resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Canseled by user", Toast.LENGTH_SHORT).show();
             }
-        });
-        return super.onCreateOptionsMenu(menu);
+        }
     }
 
     private void addUser() {
@@ -83,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
             bis.close();
         } catch (IOException e) {
             e.printStackTrace();
-            Toast.makeText(this, "Fucking Exception", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, " Exception", Toast.LENGTH_SHORT).show();
         }
 
         if (sb.equals(null))return;
@@ -107,5 +169,86 @@ public class MainActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private void loginUser() {
+        String uname , pass, pass2 = "";
+        SQLiteDatabase db = dbh.getReadableDatabase();
+
+        if (et_username.getText().toString().trim().isEmpty()){
+            et_username.setError("Empty username");
+            et_username.requestFocus();
+            return;
+        }else if (et_password.getText().toString().trim().isEmpty()){
+            et_password.setError("Empty password");
+            et_password.requestFocus();
+            return;
+        }
+
+        uname = et_username.getText().toString();
+        pass = et_password.getText().toString();
+        uname = "'" + uname + "'";
+
+        Cursor cursor = db.rawQuery("SELECT * FROM 'tb_users' WHERE username=" + uname ,null);
+        if (cursor.getCount() == 0){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Username Not Found !");
+            builder.show();
+            return;
+        }
+
+        if (cursor.moveToFirst()){
+            pass2 = cursor.getString(cursor.getColumnIndex(user.key_user_password));
+            if (pass2.equals(pass)){
+                Toast.makeText(this, "Logged in ", Toast.LENGTH_SHORT).show();
+                et_password.getText().clear();
+            }else {
+                Toast.makeText(this, "Wrong Pass", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            preferences.edit().putString(user.key_user_name,
+                    cursor.getString(cursor.getColumnIndex(user.key_user_name))).apply();
+            preferences.edit().putInt(user.key_user_id,
+                    cursor.getInt(cursor.getColumnIndex(user.key_user_id))).apply();
+            preferences.edit().putString(user.key_user_first_name,
+                    cursor.getString(cursor.getColumnIndex(user.key_user_first_name))).apply();
+            preferences.edit().putString(user.key_user_last_name,
+                    cursor.getString(cursor.getColumnIndex(user.key_user_last_name))).apply();
+            preferences.edit().putString(user.key_user_email,
+                    cursor.getString(cursor.getColumnIndex(user.key_user_email))).apply();
+            preferences.edit().putString(user.key_user_region,
+                    cursor.getString(cursor.getColumnIndex(user.key_user_region))).apply();
+        }
+        cursor.close();
+        if (db.isOpen())db.close();
+        setContentView(R.layout.main_page);
+        init_main_page_views();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("add users").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                addUser();
+                return false;
+            }
+        });
+        menu.add("log out").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (preferences.contains("username")) {
+                    preferences.edit().clear().apply();
+                    Toast.makeText(MainActivity.this, "logged out", Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.activity_main);
+                    initviews();
+                }else {
+                    Toast.makeText(MainActivity.this, "Not logged in", Toast.LENGTH_SHORT).show();
+                }
+//                preferences.edit().remove("username").apply();
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/a/ara/SignUpActivity.java
+++ app/src/main/java/com/a/ara/SignUpActivity.java
@@ -1,6 +1,11 @@
 package com.a.ara;
 
-import android.support.annotation.StringDef;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,12 +27,20 @@ public class SignUpActivity extends AppCompatActivity {
     String[] region_items ,question_items ;
     String region;
     String question;
-    Button btn_commit;
+    Button btn_commit,btn_test_username;
+    dbHelper dbh ;
+    Intent result_intent = new Intent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        setResult(RESULT_CANCELED,result_intent);
+        initviews();
+
+    }
+
+    private void initviews() {
 
         Cbox = (CheckBox) findViewById(R.id.C_Box);
         et_firstname = (EditText) findViewById(R.id.et_firstname);
@@ -39,10 +52,13 @@ public class SignUpActivity extends AppCompatActivity {
         et_birthday = (EditText) findViewById(R.id.et_birthday);
         et_nickname = (EditText) findViewById(R.id.et_nickname);
 
-        sp_region = (Spinner) findViewById(R.id.sp_region);
-        sp_question = (Spinner) findViewById(R.id.sp_question);
+        dbh = new dbHelper(this,"sho");
 
         btn_commit = (Button) findViewById(R.id.btn_commit);
+        btn_test_username = (Button) findViewById(R.id.btn_test_username);
+
+        sp_region = (Spinner) findViewById(R.id.sp_region);
+        sp_question = (Spinner) findViewById(R.id.sp_question);
 
         region_items = getResources().getStringArray(R.array.regions);
         question_items = getResources().getStringArray(R.array.questions);
@@ -97,54 +113,144 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
-//        et_username.setError("empty username");
-//        et_username.requestFocus();
+        btn_test_username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (et_username.getText().toString().equals("")){
+                    et_username.setError("Empty username");
+                    et_username.requestFocus();
+                    return;
+                }
+                if (!testUsername(et_username.getText().toString())){
+                    et_username.setError("invalid username");
+                    et_username.requestFocus();
+                    return;
+                }
+                Cursor cursor = valid_username();
+                if (cursor.getCount()>0){
+                    Toast toast = Toast.makeText(SignUpActivity.this, "Already Taken !", Toast.LENGTH_SHORT);
+                    toast.getView().setBackgroundColor(Color.rgb(200,0,0));
+                    toast.show();
+                    return;
+                }else {
+                    Toast toast = Toast.makeText(SignUpActivity.this, "Available", Toast.LENGTH_SHORT);
+                    toast.getView().setBackgroundColor(Color.rgb(0,150,0));
+                    toast.show();
+                }
+            }
+        });
 
         btn_commit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!testUsername(et_username.getText().toString())){
-                    et_username.setError("empty username");
+                //          check the edit texts
+                if (et_username.getText().toString().equals("")){
+                    et_username.setError("Empty username");
                     et_username.requestFocus();
-                }
-                if (!isValidEmailAddress(et_email.getText().toString())){
-                    et_email.setError("invalid Email Address");
-                    et_email.requestFocus();
-                }
-                if (!isValidPassword(et_password.getText().toString())){
+                    return;
+                }else if (!testUsername(et_username.getText().toString())){
+                    et_username.setError("invalid username");
+                    et_username.requestFocus();
+                    return;
+                }else if (!isValidPassword(et_password.getText().toString())){
                     et_password.setError("invalid pattern for password");
                     et_password.requestFocus();
-                }if (et_password.getText().toString().length() <8 |et_password.getText().toString().length()>50){
+                    return;
+                }else if (et_password.getText().toString().length() <8 |et_password.getText().toString().length()>50){
                     et_password.setError("password must be between 8 and 50 characters");
                     et_password.requestFocus();
-                }
-                if (!isValidBdate(et_birthday.getText().toString())){
-                    et_birthday.setError("invalid birthday format");
-                    et_birthday.requestFocus();
+                    return;
+                }else if (!isValidEmailAddress(et_email.getText().toString())){
+                    et_email.setError("invalid Email Address");
+                    et_email.requestFocus();
+                    return;
+                }else if (!Cbox.isChecked()){
+                    if (!isValidBdate(et_birthday.getText().toString())) {
+                        et_birthday.setError("invalid birthday format");
+                        et_birthday.requestFocus();
+                        return;
+                    }
                 }
 
+                //      check username availability
+                Cursor cursor = valid_username();
+                if (cursor.getCount()>0){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage("Already Taken UserName !");
+                    builder.show();
+                }else {
 
+                    Bundle result_bundle = new Bundle();
+                    ContentValues values = new ContentValues();
+                    int max_userid = 0;
+                    SQLiteDatabase db = dbh.getReadableDatabase();
+                    Cursor cursor2 = db.rawQuery("SELECT MAX(userid) FROM 'tb_users'",null);
+
+                    if (cursor2.getCount()>0) {
+                        if (cursor2.moveToFirst()) {
+                            max_userid = cursor2.getInt(cursor2.getColumnIndex("MAX(userid)"));
+                        }
+                        cursor2.close();
+//                        Toast.makeText(SignUpActivity.this, "max userid : " + max_userid, Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(SignUpActivity.this, "Error userid", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    values.put(user.key_user_id,max_userid+1);
+                    values.put(user.key_user_first_name,et_firstname.getText().toString());
+                    values.put(user.key_user_last_name,et_lastname.getText().toString());
+                    values.put(user.key_user_name,et_username.getText().toString());
+                    values.put(user.key_user_password,et_password.getText().toString());
+                    values.put(user.key_user_email,et_email.getText().toString());
+                    values.put(user.key_user_region,region);
+                    values.put(user.key_user_question,question);
+                    values.put(user.key_user_answer,et_answer.getText().toString());
+                    dbh.insert(values);
+                    cursor.close();
+                    result_bundle.putInt(user.key_user_id,values.getAsInteger(user.key_user_id));
+                    result_bundle.putString(user.key_user_first_name,values.getAsString(user.key_user_first_name));
+                    result_bundle.putString(user.key_user_last_name,values.getAsString(user.key_user_last_name));
+                    result_bundle.putString(user.key_user_name,values.getAsString(user.key_user_name));
+                    result_bundle.putString(user.key_user_password,values.getAsString(user.key_user_password));
+                    result_bundle.putString(user.key_user_email,values.getAsString(user.key_user_email));
+                    result_bundle.putString(user.key_user_region,values.getAsString(user.key_user_region));
+                    result_bundle.putString(user.key_user_question,values.getAsString(user.key_user_question));
+                    result_bundle.putString(user.key_user_answer,values.getAsString(user.key_user_answer));
+                    result_intent.putExtra("result_sign_up",result_bundle);
+                    setResult(RESULT_OK,result_intent);
+                    finish();
+                }
 
             }
-
-
         });
 
     }
-    public boolean testUsername(String username){
-        return !(username.length() > 50 | username.length() < 4 );
 
+    private Cursor valid_username() {
+        String uname;
+        uname = et_username.getText().toString();
+        uname = "'" + uname + "'";
+        SQLiteDatabase db = dbh.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT username FROM 'tb_users' WHERE username=" + uname ,null);
+        return cursor;
     }
+
+    public boolean testUsername(String username){
+        return !(username.length() > 30 | username.length() < 4 );
+    }
+
     public boolean isValidEmailAddress(String email) {
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
         java.util.regex.Matcher m = p.matcher(email);
         return m.matches();
     }
+
     public boolean isValidPassword(String password){
         String regex = "[a-zA-Z0-9@$&!]+";
         return password.matches(regex);
     }
+
     public boolean isValidBdate(String Bdate){
         String regex = "^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$";
         return Bdate.matches(regex);
